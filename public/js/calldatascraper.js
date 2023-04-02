9db81c1be229eb82aa21e7eda782c43194665ea9
Pranav Goel <pranavgoel.engineering@gmail.com>
2023-04-02 16:06:54 -0400

    download file

--- public/js/calldatascraper.js
+++ public/js/calldatascraper.js
@@ -3,7 +3,8 @@ console.log("reached");
 scrape.addEventListener("click", (e) => {
   e.preventDefault();
   console.log("clicked");
-  getData();
+  const dataprocessed = getData();
+  download();
 });
 
 async function getData() {
@@ -18,15 +19,17 @@ async function getData() {
   // zip.generateAsync({type:"blob"}).then(function (blob) { // 1) generate the zip file
   //   saveAs(blob, "hotlap_data.zip");                          // 2) trigger the download
   // });
-  if (data['dataprocessed']) {
-    console.log('downloading file');
-    // let url = window.location.host + '/download';
-    // console.log(url);
-    // window.open(url);
-    await fetch("/download");
-    const result = await response.json();
-    console.log(result)
-  }
+  return data;
+
+async function download(){
+  console.log('downloading file');
+  // let url = window.location.host + '/download';
+  // console.log(url);
+  // window.open(url);
+  await fetch("/download");
+  const result = await response.json();
+  console.log(result)
+}
   // Creating a Blob for having a csv file format
   // and passing the data with type
 //  let csvcontent = "data:text/csv;charset=utf-8," + data['reftimes'];
